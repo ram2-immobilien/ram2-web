6bfbb1fb27841c670765e0c3ac7cbfeeae6c12cb
jolte4j <jolte4j@gmail.com>
2026-03-19 19:41:48 +0100

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,16 @@
 "use client"
 
 import { useTranslations } from "next-intl";
+import SliderMultiple from "@/components/Slider-multiple/SliderMultiple";
 
 export default function Home() {
     const t = useTranslations("home")
 
     return (
-        <p>{t("header")}</p>
+ <div>
+            <p>{t("header")}</p>
+
+            <SliderMultiple />
+</div>
     );
 }
