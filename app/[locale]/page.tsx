d4a8cb0bb906eb567697498be2731330b11348aa
jolte4j <jolte4j@gmail.com>
2026-03-20 17:58:03 +0100

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,16 +1,27 @@
 "use client"
 
+import { useState } from "react";
 import { useTranslations } from "next-intl";
 import SliderMultiple from "@/components/Slider-multiple/SliderMultiple";
 
 export default function Home() {
     const t = useTranslations("home")
 
+    const [min, setMin] = useState(0);
+    const [max, setMax] = useState(2000000);
+
     return (
  <div>
             <p>{t("header")}</p>
 
-            <SliderMultiple />
+            <SliderMultiple       
+        min={min}
+        max={max}
+        setMin={setMin}
+        setMax={setMax}
+      />
+
+
 </div>
     );
 }
