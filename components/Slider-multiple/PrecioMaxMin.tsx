eff055c44a42d5e3d2cb2649f98248d38befc45d
jolte4j <jolte4j@gmail.com>
2026-03-20 17:58:06 +0100

    Create PrecioMaxMin.tsx

---
+++ components/Slider-multiple/PrecioMaxMin.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react"
+export default function PrecioMaxMin() {
+
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(2000000);
+<div>
+
+      </div>
+}
