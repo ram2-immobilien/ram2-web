037837c3e73d4547d46cd279c8e125380ff65fd0
Willygabs <gabsxp11@gmail.com>
2026-03-25 15:31:33 +0100

    Update Primary-Button.tsx

--- components/PrimaryButton/Primary-Button.tsx
+++ components/PrimaryButton/Primary-Button.tsx
@@ -1,33 +1,22 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import "./PrimaryButton.css";
 
 type PrimaryButtonProps = {
   text: string;
-  onClick?: () => void;
-  icon?: "search" | "arrow" | "none";
+  onClick: () => void;
 };
 
 export default function PrimaryButton({
   text,
   onClick,
-  icon = "search",
 }: PrimaryButtonProps) {
-  const renderIcon = () => {
-    switch (icon) {
-      case "search":
-        return <FontAwesomeIcon icon={faSearch} />;
-      case "arrow":
-        return <FontAwesomeIcon icon={faArrowRight} />;
-      default:
-        return null;
-    }
-  };
-
   return (
     <button className="primary-button" onClick={onClick}>
       <span>{text}</span>
-      {icon !== "none" && <span className="icon">{renderIcon()}</span>}
+      <span className="icon">
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+      </span>
     </button>
   );
 }
