cd3098f0e92d6cec5f226c8e9d864a98231f7aeb
Willygabs <gabsxp11@gmail.com>
2026-03-25 16:45:52 +0100

    Update Primary-Button.tsx
    Finalizado

--- components/PrimaryButton/Primary-Button.tsx
+++ components/PrimaryButton/Primary-Button.tsx
@@ -1,21 +1,23 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import "./PrimaryButton.css";
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import "./Primary-Button.css";
 
 type PrimaryButtonProps = {
   text: string;
   onClick: () => void;
+  icon: IconDefinition
 };
 
 export default function PrimaryButton({
   text,
   onClick,
+  icon,
 }: PrimaryButtonProps) {
   return (
     <button className="primary-button" onClick={onClick}>
       <span>{text}</span>
       <span className="icon">
-        <FontAwesomeIcon icon={faMagnifyingGlass} />
+       <FontAwesomeIcon icon={icon} /> 
       </span>
     </button>
   );
