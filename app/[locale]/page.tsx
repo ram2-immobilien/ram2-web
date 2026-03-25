5d77be817afc29aaea9280c524dce477cbe75247
a21203 <ax969xa@gmail.com>
2026-03-25 04:49:22 +0100

    update CSS to match Figma design

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,17 @@
 "use client"
-
-import { useTranslations } from "next-intl";
+import { useState } from "react";
+import TextField from "@/components/TextField/TextField";
+import TextArea from "@/components/TextArea/TextArea";
 
 export default function Home() {
-    const t = useTranslations("home")
+  const [nombre, setNombre] = useState("");
+  const [mensaje, setMensaje] = useState("");
 
-    return (
-        <p>{t("header")}</p>
-    );
+  return (
+    <div style={{ padding: "40px", maxWidth: "800px" }}>
+      <TextField label="Nombre completo" placeholder="Nombre completo" value={nombre} setValue={setNombre} />
+      <br />
+      <TextArea label="Mensaje" placeholder="Mensaje" value={mensaje} setValue={setMensaje} />
+    </div>
+  );
 }
